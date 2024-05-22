609398225fda76df10a01a2c0da0502c5d927ea8
kumar8317 <ankitkumar8317@gmail.com>
2024-05-22 20:44:49 +0530

    Feat (error middleware): Finish error handling

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import cors from "cors";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
 import { PrismaClient } from "@prisma/client";
+import { errorMiddleware } from "./middlewares/errors";
 
 const app: Express = express();
 
@@ -20,32 +21,8 @@ export const prismaClient = new PrismaClient({
   log: ["query"],
 });
 
-app.use("*", (req: Request, res: Response, next: NextFunction) => {
-  const error = {
-    status: 404,
-    message: "Api endpoint not found",
-  };
-  next(error);
-});
+app.use(errorMiddleware)
 
-const errorHanlder: ErrorRequestHandler = (
-  err,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  console.log(err);
-  const status = err.status || 500;
-  const message = err.message || "Something went wrong";
-  const data = err.data || null;
-  res.status(status).json({
-    type: "error",
-    message,
-    data,
-  });
-};
-// global error handling middleware
-app.use(errorHanlder);
 app.listen(PORT, () => {
   console.log(`App working!`);
 });
